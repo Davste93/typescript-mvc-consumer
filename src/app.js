62d34bc504a0afd1c9ecbf0091d4f13c79f5cfcb
dstellini <davste93@gmail.com>
2016-02-29 19:19:09 +0100

    Deleted unecessary files

--- src/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var config_1 = require("./implementation/config/config");
-var user = config_1.Services.userService.getCompleteUser('1').then(function (user) {
-    console.log(user.addresses);
-});
-//# sourceMappingURL=app.js.map
--- src/implementation/data/AddressDataRepository.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=AddressDataRepository.js.map
