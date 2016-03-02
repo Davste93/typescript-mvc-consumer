312f94c16d150102e27b09df20fbf401c2a19e54
dstellini <davste93@gmail.com>
2016-03-02 01:04:53 +0100

    removed files

--- node_modules/tsmvc/src/interfaces/service/Service.ts
+++
@@ -1,9 +0,0 @@
-import {Inject, TypeBinding, Kernel, TypeBindingScopeEnum} from "inversify";
-export interface Service {
-
-  // dependencyList : List< IDataListAsync<IModel, IData<IModel>>>;
-  //
-  //   registerDependency <BaseType extends IModel,
-  //     DataLayerType extends IDataListAsync<BaseType, IData<BaseType>>>
-  //     (dataLayer : IDataListAsync<BaseType, IData<BaseType>>);
-}
